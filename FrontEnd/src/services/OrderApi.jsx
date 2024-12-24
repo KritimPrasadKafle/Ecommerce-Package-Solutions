75bc9ced8d424df70b2eda81c1c18967a3eaaf9e
KritimPrasadKafle <kritim10k@gmail.com>
2024-12-24 20:15:04 +0545

    ChangedPortNumberInFrontEnd

--- FrontEnd/src/services/OrderApi.jsx
+++ FrontEnd/src/services/OrderApi.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Returns the packages from the server's response if successful, or throws an error if the request fails.
 export const placeOrder = async (selectedItems) => {
   try {
-    const response = await axios.post('http://localhost:5000/api/v1/order', { selectedItems });
+    const response = await axios.post('http://localhost:3000/api/v1/order', { selectedItems });
     console.log("API response:", response.data.packages);
     return response.data.packages;
 
